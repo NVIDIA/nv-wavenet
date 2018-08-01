a6c105f3922b3093928e99471cf098c664865dd9
Levs Dolgovs <ldolgovs@nvidia.com>
2018-08-01 16:43:12 -0700

    Add pause/resume and CPU-asynchronous generation
    The change allows the kernels to reuse last activations when invoked on subsequent chunks.
    The host code has a new interface, nvWavenetInfer::run_chunks(...), which accepts
    a lambda callback for any CPU postprocessing, one chunk per call (encoding, sending
    over the network, etc.). Performance of non-chunked codepath is unaffected, and
    the per-chunk overhead is negligible for realistic chunk sizes.
    However, the whole activation tensors still need to be allocated on the GPU.

--- nv_wavenet_perf.cu
+++ nv_wavenet_perf.cu
@@ -26,12 +26,13 @@
  ******************************************************************************/
 
 #include "nv_wavenet.cuh"
+#include <cuda_profiler_api.h>
 #include <stdio.h>
 #include <vector>
 #include <unistd.h>
 
 template <typename T_weight, typename T_data, int R, int S, int A>
-float getSampleRateT(int num_layers, int max_dilation, int batch_size, int batch_size_per_block, int num_samples, int mode) {
+float getSampleRateT(int num_layers, int max_dilation, int batch_size, int batch_size_per_block, int num_samples, int num_samples_per_chunk, int mode) {
 
     // Set up initial activations
 
@@ -67,7 +68,13 @@ float getSampleRateT(int num_layers, int max_dilation, int batch_size, int batch
     gpuErrChk(cudaEventCreate(&start));
     gpuErrChk(cudaEventCreate(&stop));
     gpuErrChk(cudaEventRecord(start));
-    bool success = infer.run(num_samples,batch_size, NULL, batch_size_per_block);
+    int* mcYout;
+    // because the chunked version copies repeatedly, we should measure it as well.
+    gpuErrChk(cudaMallocHost(&mcYout, num_samples*batch_size*sizeof(int)));
+    cudaProfilerStart();
+    bool success = infer.run_chunks(num_samples_per_chunk, [](int*, int, int){}, num_samples, batch_size, mcYout, batch_size_per_block);
+    gpuErrChk(cudaFreeHost(mcYout));
+
     gpuErrChk(cudaEventRecord(stop));
 
     gpuErrChk(cudaEventSynchronize(stop));
@@ -80,36 +87,36 @@ float getSampleRateT(int num_layers, int max_dilation, int batch_size, int batch
 
 }
 
-float getSampleRate(int precision, int r, int s, int a, int num_layers, int max_dilation, int batch_size, int batch_size_per_block, int num_samples, int mode) {
+float getSampleRate(int precision, int r, int s, int a, int num_layers, int max_dilation, int batch_size, int batch_size_per_block, int num_samples, int num_samples_per_chunk, int mode) {
     assert(a==256);
     float sample_rate;
     if (r == 32) {
         assert(s==128);
         assert(a==256);
         if (precision == 16) {
-                sample_rate = getSampleRateT<half2,half,32,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+	    sample_rate = getSampleRateT<half2,half,32,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
         }
         else {
             assert(precision==32);
-                sample_rate = getSampleRateT<float,float,32,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+	    sample_rate = getSampleRateT<float,float,32,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
         }
     }
     else {
         assert(r==64);
         if (precision == 16) {
             if (s==128) 
-                sample_rate = getSampleRateT<half2,half,64,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+                sample_rate = getSampleRateT<half2,half,64,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
             else if (s==256)
-                sample_rate = getSampleRateT<half2,half,64,256,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+                sample_rate = getSampleRateT<half2,half,64,256,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
             else
                 assert(false);
         }
         else {
             assert(precision==32);
             if (s==128) 
-                sample_rate = getSampleRateT<float,float,64,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+                sample_rate = getSampleRateT<float,float,64,128,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
             else if (s==256)
-                sample_rate = getSampleRateT<float,float,64,256,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+                sample_rate = getSampleRateT<float,float,64,256,256>(num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
             else
                 assert(false);
         }
@@ -146,9 +153,10 @@ int main(int argc, char* argv[]) {
     int max_dilation = 512;
     int mode = 0;
     int precision = 16;
+    int num_samples_per_chunk = 2048;
 
     int c;
-    while ((c = getopt (argc, argv, "l:r:s:a:b:n:c:d:m:p:")) != -1) {
+    while ((c = getopt (argc, argv, "l:r:s:a:b:n:c:d:m:p:t:")) != -1) {
         switch (c) {
             case 'l':
                 num_layers = atoi(optarg);
@@ -179,6 +187,9 @@ int main(int argc, char* argv[]) {
                 break;
             case 'p':
                 precision = atoi(optarg);
+                break;
+            case 't':
+                num_samples_per_chunk = atoi(optarg);
                 break;
             default:
                 assert(false);
@@ -216,6 +227,6 @@ int main(int argc, char* argv[]) {
 
     srand(1);
 
-    float sample_rate = getSampleRate(precision, r, s, a, num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, mode);
+    float sample_rate = getSampleRate(precision, r, s, a, num_layers, max_dilation, batch_size, batch_size_per_block, num_samples, num_samples_per_chunk, mode);
     printf("Sample rate: %f kHz\n", sample_rate);
 }
